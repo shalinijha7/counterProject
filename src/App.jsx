931cb6a5dc0413f722f37862d6ecdf94e2e75b4c
Shalini Jha <shalini16jha@gmail.com>
2024-11-08 17:57:03 +0530

    finalProject

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let  [counter, setCounter] = useState(15)
+  // let counter = 15
+  const addValue = () => {
+    console.log("clicked", counter)
+    if(counter >= 20){
+      return
+    }
+    counter = counter + 1
+    setCounter(counter)
+  }
+
+  const removeValue = () => {
+    console.log("clicked", counter)
+    if(counter <= 0 ){
+      return 
+    }
+    counter = counter - 1
+    setCounter(counter)
+  }
+
+  return (
+    <>
+    <h1>Counter app</h1>  
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>Add value: {counter}</button>
+    <br />
+    <button onClick={removeValue}>Remove value: {counter}</button>
+    </>
+  )
+}
+
+export default App
